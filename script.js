5ee717bb7b59d46ef7d2b93050314c61ca9e05f6
Jose Eduardo Payamps <josenxs12@gmail.com>
2023-11-08 17:55:26 -0500

    Added logic for Expenses Tracking App

--- script.js
+++ script.js
@@ -1,1 +1,88 @@
-if ()
+let expenses = [];
+let totalAmount = 0;
+
+const categorySelect = document.getElementById('category-select');
+const amountInput = document.getElementById('amount-input');
+const dateInput = document.getElementById('date-input');
+const addBtn = document.getElementById('add-btn');
+const expensesTableBody = document.getElementById('expenses-table-body');
+const totalAmountCell = document.getElementById('total-amount');
+
+addBtn.addEventListener('click', function(){
+  const category = categorySelect.value;
+  const amount = Number(amountInput.value);
+  const date = dateInput.value;
+
+  //if no category is chose
+  if (category === ''){
+    alert('Please select a category');
+    return;
+  }
+  //if the amount is less than 0 or is not a number
+  if (isNaN(amount) || amount <= 0) {
+    alert('Please enter valid amount');
+    return;
+  }
+  //if the user does not select a date
+  if (date === ''){
+    alert('Please select a date');
+    return;
+  }
+  expenses.push({category, amount, date});
+
+  totalAmount += amount;
+  totalAmountCell.textContent = totalAmount;
+
+  const newRow = expensesTableBody.insertRow();
+
+  const catetegoryCell = newRow.insertCell();
+  const amountCell = newRow.insertCell();
+  const dateCell = newRow.insertCell();
+  const deleteCell = newRow.insertCell();
+  const deleteBtn = document.createElement('button');
+
+  deleteBtn.textContent = 'Delete';
+  deleteBtn.classList.add('delete-btn');
+  deleteBtn.addEventListener('click', function(){
+    expenses.splice(expenses.indexOf(expense), 1);
+
+    totalAmount -= expense.amount;
+    totalAmountCell.textContent = totalAmount;
+
+    expensesTableBody.removeChild(newRow);
+  });
+
+  const expense = expenses[expenses.length - 1];
+  catetegoryCell.textContent = expense.category;
+  amountCell.textContent = expense.amount;
+  dateCell.textContent = expense.date;
+  deleteCell.appendChild(deleteBtn);
+
+});
+
+for (const expense of expenses){
+  totalAmount += expense.amount;
+  totalAmountCell.textContent = totalAmount;
+
+  const newRow = expensesTableBody.insertRow();
+  const catetegoryCell = newRow.insertCell();
+  const amountCell = newRow.insertCell();
+  const dateCell = newRow.insertCell();
+  const deleteCell = newRow.insertCell();
+  const deleteBtn = document.createElement('button');
+  deleteBtn.textContent = 'Delete';
+  deleteBtn.classList.add('delete-btn');
+  deleteBtn.addEventListener('click', function(){
+    expenses.splice(expenses.indexOf(expense), 1);
+
+    totalAmount -= expense.amount;
+    totalAmountCell.textContent = totalAmount;
+
+    expensesTableBody.removeChild(newRow);
+  });
+  catetegoryCell.textContent = expense.category;
+  amountCell.textContent = expense.amount;
+  dateCell.textContent = expense.date;
+  deleteCell.appendChild(deleteBtn);
+
+}
